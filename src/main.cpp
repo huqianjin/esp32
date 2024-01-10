ce00a164b808480330d7825275de40ea502af153
huqianjin <hqianjin@qq.com>
2024-01-10 20:00:45 +0800

    tt

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <HardwareSerial.h>
+#include <TFT_eSPI.h>
 
+TFT_eSPI tft =TFT_eSPI();
+int i = 0 ;
 
-
-int i=0;
-
-void setup() {
-
-  Serial.begin(115200);          
-  Serial.print(i++);
-  delay(2000);
-
-
+void setup(void) {
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLUE);
+  tft.setCursor(20, 10);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.print("Hello ST7735!");
 }
 
-
 void loop() {
-
-
  Serial.print(i++);
 
 
     delay(2000);
+
+
 }
